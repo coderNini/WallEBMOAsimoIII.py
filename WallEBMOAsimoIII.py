c897456a5f1b33e769959ea50866c765302d4f29
coderNini <99504328+coderNini@users.noreply.github.com>
2022-09-12 14:49:12 -0700

    Create WallEBMOAsimoIII.py

---
+++ WallEBMOAsimoIII.py
@@ -0,0 +1,96 @@
+ #BEGIN CODE FOR DANCE OFF
+    
+    def _gopigo3_command_dance_off(self):
+        beat = 60/130
+        self.gopigo3.set_speed(1000)
+      
+        self.gopigo3.spin_left()
+        sleep(1*beat)
+        self.gopigo3.spin_right()
+        sleep(2*beat)
+        self.gopigo3.spin_left()
+        sleep(2*beat)
+        self.gopigo3.spin_right()
+        sleep(1.3*beat)
+        
+        
+        self.gopigo3.forward()
+        sleep(0.5*beat)
+        self.gopigo3.backward()
+        sleep(1*beat)
+        self.gopigo3.forward()
+        sleep(0.5*beat)
+        
+        
+        self.gopigo3.steer(10,1000)
+        sleep(2)
+        self.gopigo3.drive_cm(5)
+        sleep(1.35*beat)
+        self.gopigo3.drive_cm(3)
+        sleep(0.5*beat)
+        self.gopigo3.drive_cm(3)
+        sleep(0.5*beat)
+       
+    
+      
+        self.gopigo3.spin_left()
+        sleep(1.25*beat)
+        self.gopigo3.drive_cm(3)
+        sleep(0.35*beat)
+        self.gopigo3.drive_cm(3)
+        sleep(0.35*beat)
+        
+        
+        self.gopigo3.spin_left()
+        sleep(0.5*beat)
+        self.gopigo3.drive_cm(-5)
+        sleep(1*beat)
+        self.gopigo3.drive_cm(-3)
+        sleep(0.5*beat)
+        self.gopigo3.drive_cm(-3)
+        sleep(0.5*beat)
+        self.gopigo3.drive_cm(-3)
+        #sleep(0.5*beat)
+        self.gopigo3.steer(1000,10)
+        sleep(3*beat)
+        self.gopigo3.spin_right()
+        sleep(1.2*beat)
+        self.gopigo3.stop()
+        sleep(3.65*beat)
+        
+        
+        self.gopigo3.forward()
+        sleep(1.15*beat)
+        self.gopigo3.backward()
+        sleep(1*beat)
+        self.gopigo3.forward()
+        sleep(1*beat)
+        self.gopigo3.spin_right()
+        sleep(2.5*beat)
+        self.gopigo3.stop()
+        sleep(1.5*beat)
+        self.gopigo3.forward()
+        sleep(2*beat)
+        self.gopigo3.backward()
+        sleep(2*beat)
+        self.gopigo3.spin_right()
+        sleep(2.5*beat)
+        self.gopigo3.stop()
+        sleep(1.5*beat)
+        self.gopigo3.backward()
+        sleep(2*beat)
+        self.gopigo3.forward()
+        sleep(2*beat)
+        self.gopigo3.spin_right()
+        sleep(2.5*beat)
+        self.gopigo3.stop()
+        sleep(1.5*beat)
+        
+        self.gopigo3.spin_right()
+        sleep(1*beat)
+        self.gopigo3.stop()
+        sleep(1*beat)
+        self.gopigo3.forward()
+        sleep(1*beat)
+        
+        #START TURNING AROUND THE CIRCLE TO END IN OOPPOOSITE POSITIONS OF WHERE WE STARTED
